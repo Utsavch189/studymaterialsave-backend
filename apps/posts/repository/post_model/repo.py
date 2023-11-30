6f91adf1cab2be8bd7d3ceb25b92581031a3a209
Utsav <utsavpokemon9000chatterjee@gmail.com>
2023-11-30 09:14:54 +0530

    modify post meta repo

--- apps/posts/repository/post_model/repo.py
+++ apps/posts/repository/post_model/repo.py
@@ -46,12 +46,12 @@ class PostModelRepo(models.Manager,I_PostModelRepo):
                     if row[5]!=None:
                         _postdata.post_meta.append(
                             PostMetaData(
-                                post_file_meta_id=row[5],
-                                file_name=row[6],
-                                file_url=row[7],
-                                public_id=row[8],
-                                resource_type=row[9],
-                                types=row[10]
+                                post_file_meta_id=row[6],
+                                file_name=row[7],
+                                file_url=row[8],
+                                public_id=row[9],
+                                resource_type=row[10],
+                                types=row[11]
                             )
                         )
                     
